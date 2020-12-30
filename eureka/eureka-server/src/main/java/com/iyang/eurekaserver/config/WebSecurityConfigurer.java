46957645cfb21464ed3598e27cc451df72e75a15
baoyang23 <1411091515@qq.com>
2020-12-30 11:56:20 +0800

    feat:init eureka server project.

---
+++ eureka/eureka-server/src/main/java/com/iyang/eurekaserver/config/WebSecurityConfigurer.java
@@ -0,0 +1,21 @@
+package com.iyang.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Yang
+ * 当前服务 : eureka-server
+ * @date 2020/12/30 / 11:15
+ */
+
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
---
+++ eureka/eureka-server/src/test/java/com/iyang/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.iyang.eurekaserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
